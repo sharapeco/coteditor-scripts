adb6eacf204f37db99dc284993a109ac11afef8c
Shinichiro Yabu <scin.ichi@gmail.com>
2019-09-15 21:19:52 +0900

    Finished toggle-comment

--- toggle-comment.js
+++ toggle-comment.js
@@ -3,11 +3,12 @@
 
 // Settings
 const CommentStyles = [
-	{mark: "# ", lang: ["Apache", "CoffeeScript", "Git Ignore", "Makefile", "Perl", "Ruby", "Shell Script"]},
-	{mark: "// ", lang: ["C++", "CSS", "Java", "JavaScript", "PHP", "Swift"]},
-	{mark: "-- ", lang: ["SQL"]},
-	{mark: "; ", lang: ["Lisp"]},
+	{mark: "#", lang: ["Apache", "CoffeeScript", "Git Ignore", "Makefile", "Perl", "Ruby", "Shell Script"]},
+	{mark: "//", lang: ["C++", "CSS", "Java", "JavaScript", "PHP", "Swift"]},
+	{mark: "--", lang: ["SQL"]},
+	{mark: ";", lang: ["Lisp"]},
 ];
+const SP = " ";
 
 const app = Application("CotEditor");
 app.includeStandardAdditions = true;
@@ -25,14 +26,42 @@ function toggleComment(doc) {
 
 	const [begin, end] = extendSelection(doc);
 
+	const {selection} = doc;
 	const newline = newlineOfDocument(doc);
 	const lines = doc.contents().substring(begin, end).split(newline);
-	console.log(`selection has ${lines.length} lines`);
-
-	const {selection} = doc;
+	
+	// 長すぎる行で処理が重くなるのを防ぐため80文字までに制限する
+	const colLimit = 80;
+	const linesHasComment = lines.some(line => line.substring(0, colLimit).trim().indexOf(mark) === 0);
+	let newContents;
+	if (linesHasComment) {
+		newContents = lines.map(line => {
+			const spacesMatch = line.match(/^\s+/);
+			const markPos = line.indexOf(mark, spacesMatch ? spacesMatch[0].length : 0);
+			if (markPos >= 0) {
+				const hasExtraSpace = (line[markPos + mark.length] === SP);
+				const begin = markPos + mark.length + (hasExtraSpace ? 1 : 0);
+				line = line.substring(0, markPos) + line.substring(begin);
+			}
+			return line;
+		}).join(newline);
+	} else {
+		const getCommonIndent = lines => {
+			if (lines.length < 1 || lines[0].length < 1 || /[^\s]/.test(lines[0][0])) {
+				return "";
+			}
+			const indent = lines[0][0];
+			if (lines.every(line => line.indexOf(indent) === 0)) {
+				return indent + getCommonIndent(lines.map(line => line.substring(1)));
+			} else {
+				return "";
+			}
+		};
+		const indent = getCommonIndent(lines);
+		newContents = lines.map(line => indent + mark + SP + line.substring(indent.length)).join(newline);
+	}
 	selection.range = [begin, end - begin];
-
-// 	selection.contents = 
+	selection.contents = newContents;
 }
 
 function extendSelection(doc) {
